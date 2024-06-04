9e43f541b24fa5d8991008b4979ba7bfad6761cd
Fernando Isaac Gonzalez Medina <isaacgonzalez96@aragon.unam.mx>
2024-06-04 17:59:06 -0600

    .

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamMenu = document.querySelector('.header__main-ham-menu');
-    const hamMenuClose = document.querySelector('.header__main-ham-menu-close');
-    const smMenu = document.querySelector('.header__sm-menu');
-  
-    hamMenu.addEventListener('click', function () {
-      smMenu.classList.add('header__sm-menu--open');
-      hamMenu.classList.add('d-none');
-      hamMenuClose.classList.remove('d-none');
-    });
-  
-    hamMenuClose.addEventListener('click', function () {
-      smMenu.classList.remove('header__sm-menu--open');
-      hamMenu.classList.remove('d-none');
-      hamMenuClose.classList.add('d-none');
-    });
+  const hamMenu = document.querySelector('.header__main-ham-menu');
+  const hamMenuClose = document.querySelector('.header__main-ham-menu-close');
+  const smMenu = document.querySelector('.header__sm-menu');
+
+  hamMenu.addEventListener('click', function () {
+    smMenu.classList.add('header__sm-menu--open');
+    hamMenu.classList.add('d-none');
+    hamMenuClose.classList.remove('d-none');
   });
-  
+
+  hamMenuClose.addEventListener('click', function () {
+    smMenu.classList.remove('header__sm-menu--open');
+    hamMenu.classList.remove('d-none');
+    hamMenuClose.classList.add('d-none');
+  });
+});
