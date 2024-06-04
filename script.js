8bd27d28536a42346ddd6fac4e4a5ab6f1af75d4
Fernando Isaac Gonzalez Medina <isaacgonzalez96@aragon.unam.mx>
2024-06-04 14:27:12 -0600

    .

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Aquí puedes añadir cualquier interactividad que desees
-    console.log("¡JavaScript está funcionando!");
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const hamMenu = document.querySelector('.header__main-ham-menu');
+    const hamMenuClose = document.querySelector('.header__main-ham-menu-close');
+    const smMenu = document.querySelector('.header__sm-menu');
+  
+    hamMenu.addEventListener('click', function () {
+      smMenu.classList.add('header__sm-menu--open');
+      hamMenu.classList.add('d-none');
+      hamMenuClose.classList.remove('d-none');
+    });
+  
+    hamMenuClose.addEventListener('click', function () {
+      smMenu.classList.remove('header__sm-menu--open');
+      hamMenu.classList.remove('d-none');
+      hamMenuClose.classList.add('d-none');
+    });
+  });
+  
