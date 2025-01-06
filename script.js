0954ce7932d9f0cb6bea67c5f70528208721c0a9
Fernando Isaac Gonzalez Medina <isaacgonzalez96@aragon.unam.mx>
2025-01-06 09:50:27 -0600

    Actualización de Portafolio

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const hamMenu = document.querySelector('.header__main-ham-menu');
   const hamMenuClose = document.querySelector('.header__main-ham-menu-close');
   const smMenu = document.querySelector('.header__sm-menu');
+  const menuLinks = document.querySelectorAll('.header__sm-menu-link a');
+
+  function closeMenu() {
+    smMenu.classList.remove('header__sm-menu--open');
+    hamMenu.classList.remove('d-none');
+    hamMenuClose.classList.add('d-none');
+  }
 
   hamMenu.addEventListener('click', function () {
     smMenu.classList.add('header__sm-menu--open');
@@ -9,9 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     hamMenuClose.classList.remove('d-none');
   });
 
-  hamMenuClose.addEventListener('click', function () {
-    smMenu.classList.remove('header__sm-menu--open');
-    hamMenu.classList.remove('d-none');
-    hamMenuClose.classList.add('d-none');
+  hamMenuClose.addEventListener('click', closeMenu);
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
   });
 });
